e10d7cfcab1496a3d79d42bec7f20628837063c5
Juan C <juanceballos@pursuit.org>
2020-01-25 19:24:26 -0500

    added unit test

--- Horoscope-UserDefaults/Horoscope-UserDefaultsTests/Horoscope_UserDefaultsTests.swift
+++ Horoscope-UserDefaults/Horoscope-UserDefaultsTests/Horoscope_UserDefaultsTests.swift
@@ -9,26 +9,31 @@
 import XCTest
 @testable import Horoscope_UserDefaults
 
+struct CreatedHoroscope: Codable   {
+    let sungsign: String
+    let date: String
+    let horoscope: String
+}
+
+
 class Horoscope_UserDefaultsTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    //arrange
+    func testFetchHoroscope() {
+        let horoscope = "gemini"
+        let exp = XCTestExpectation(description: "Correct Horoscope")
+        let expectedSign = "Gemini"
+        
+        //act
+        HoroscopeAPI.fetchHoroscope(horoscope: horoscope) { (result) in
+            switch result   {
+            case .failure(let appError):
+                print(appError)
+            case .success(let horoscope):
+                exp.fulfill()
+                XCTAssertEqual(horoscope.sunsign, expectedSign, "expectedSign matches get request sign")
+            }
         }
+        wait(for: [exp], timeout: 5)
     }
-
+    
 }
